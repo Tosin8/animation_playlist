e4c4545ddd2a223acf6a658bf7c627afcc34711a
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-15 15:56:35 +0100

    Update star.dart

--- lib/implict_animation/star.dart
+++ lib/implict_animation/star.dart
@@ -23,8 +23,11 @@ class _StarState extends State<Star> {
       ),
       body: Column(
         children: [
-          Container(
+          AnimatedContainer(
             width: _bigger ? 100 : 500,
+            duration: const Duration(seconds: 3),
+            child: Image.network(
+                'https://images.unsplash.com/photo-1671600939110-7b0625c25ef5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80'),
           ),
           ElevatedButton(
             onPressed: () => setState(() {
