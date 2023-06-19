9658fb0b1d9cb7142c6f708bdc8f4fcfced2d7bd
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-19 21:23:24 +0100

    Changing Container to Animated Container
    Using Implicit Animation

--- lib/vandad/implicit_animation/zoom_effect.dart
+++ lib/vandad/implicit_animation/zoom_effect.dart
@@ -15,45 +15,46 @@ class _Zoom_EffectState extends State<Zoom_Effect> {
   var _buttonTitle = 'Zoom In';
 
   var _width = defaultWidth;
+  var _curve = Curves.bounceOut;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Zoom Effect',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w500,
-            ),
+      appBar: AppBar(
+        title: const Text(
+          'Zoom Effect',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
           ),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
-          elevation: 0,
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 150.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: _width,
-                child: Image.network(
-                    'https://images.unsplash.com/photo-1686824579901-9c90fd95b086?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _isZoomedIn = !_isZoomedIn;
-                      _buttonTitle = _isZoomedIn ? 'Zoom Out' : 'Zoom In';
-                      _width = _isZoomedIn
-                          ? MediaQuery.of(context).size.width
-                          : defaultWidth;
-                    });
-                  },
-                  child: Text(_buttonTitle)),
-            ],
-          ),
-        ));
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        elevation: 0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            AnimatedContainer(
+              width: _width,
+              child: Image.network(
+                  'https://images.unsplash.com/photo-1686824579901-9c90fd95b086?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80'),
+            ),
+          ]),
+          const SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _isZoomedIn = !_isZoomedIn;
+                  _buttonTitle = _isZoomedIn ? 'Zoom Out' : 'Zoom In';
+                  _width = _isZoomedIn
+                      ? MediaQuery.of(context).size.width
+                      : defaultWidth;
+                });
+              },
+              child: Text(_buttonTitle)),
+        ],
+      ),
+    );
   }
 }
