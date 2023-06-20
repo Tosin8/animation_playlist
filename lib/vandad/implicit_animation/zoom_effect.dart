0719b48ebe679ce37f45b547c2bdf578f47f8613
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-20 20:06:59 +0100

    Update zoom_effect.dart

--- lib/vandad/implicit_animation/zoom_effect.dart
+++ lib/vandad/implicit_animation/zoom_effect.dart
@@ -19,28 +19,37 @@ class _Zoom_EffectState extends State<Zoom_Effect> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Zoom Effect',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w500,
+        appBar: AppBar(
+          title: const Text(
+            'Zoom Effect',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
           ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          elevation: 0,
         ),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        elevation: 0,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
             AnimatedContainer(
               duration: const Duration(milliseconds: 370),
               curve: _curve,
               width: _width,
               child: Image.network(
-                  'https://images.unsplash.com/photo-1686824579901-9c90fd95b086?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80'),
+                'https://images.unsplash.com/photo-1686824579901-9c90fd95b086?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80',
+                frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+                  return child;
+                },
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
             ),
           ]),
           const SizedBox(height: 20),
@@ -56,8 +65,6 @@ class _Zoom_EffectState extends State<Zoom_Effect> {
                 });
               },
               child: Text(_buttonTitle)),
-        ],
-      ),
-    );
+        ]));
   }
 }
