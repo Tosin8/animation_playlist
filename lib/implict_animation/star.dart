a51546fde1ed063919feffe9517d446e5ecc8acb
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-20 20:53:57 +0100

    Updating the star file

--- lib/implict_animation/star.dart
+++ lib/implict_animation/star.dart
@@ -1,42 +1,49 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class Star extends StatefulWidget {
-//   const Star({super.key});
+class Star extends StatefulWidget {
+  const Star({super.key});
 
-//   @override
-//   State<Star> createState() => _StarState();
-// }
+  @override
+  State<Star> createState() => _StarState();
+}
 
-// class _StarState extends State<Star> {
-//   // ignore: prefer_typing_uninitialized_variables
+const defaultWidth = 300.0;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.blue,
-//         title: const Text(
-//           'Animated Container',
-//           style: TextStyle(fontWeight: FontWeight.w400),
-//         ),
-//         centerTitle: true,
-//       ),
-//       body: Column(
-//         children: [
-//           AnimatedContainer(
-//             width: _bigger ? 100 : 500,
-//             duration: const Duration(seconds: 3),
-//             child: Image.network(
-//                 'https://images.unsplash.com/photo-1671600939110-7b0625c25ef5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80'),
-//           ),
-//           ElevatedButton(
-//             onPressed: () => setState(() {
-//               _bigger = !_bigger;
-//             }),
-//             child: const Icon(Icons.star),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
+class _StarState extends State<Star> {
+  // ignore: prefer_typing_uninitialized_variables
+
+  var _bigger = false;
+  var _width = defaultWidth;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          'Animated Container',
+          style: TextStyle(fontWeight: FontWeight.w400),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            AnimatedContainer(
+              width: _bigger ? _width : MediaQuery.of(context).size.width,
+              duration: const Duration(milliseconds: 370),
+              child: Image.network(
+                  'https://images.unsplash.com/photo-1671600939110-7b0625c25ef5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80'),
+            ),
+          ]),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () => setState(() {
+              _bigger = !_bigger;
+            }),
+            child: const Icon(Icons.cake),
+          )
+        ],
+      ),
+    );
+  }
+}
