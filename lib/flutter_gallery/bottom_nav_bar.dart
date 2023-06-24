5c943698362c026002dd0be64b8637a28e83c1c6
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-24 22:27:53 +0100

    Update bottom_nav_bar.dart

---
+++ lib/flutter_gallery/bottom_nav_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bottom_Nav_1 extends StatefulWidget {
+  const Bottom_Nav_1({super.key});
+
+  @override
+  State<Bottom_Nav_1> createState() => _Bottom_Nav_1State();
+}
+
+class _Bottom_Nav_1State extends State<Bottom_Nav_1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
