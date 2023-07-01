ad12b10de471625e7c861b4d325058389a315f99
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-01 21:19:05 +0100

    Update bottom_nav.dart

--- lib/bottom_nav.dart
+++ lib/bottom_nav.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Nav_Bar extends StatefulWidget {
   const Nav_Bar({super.key});
@@ -11,10 +12,32 @@ class _Nav_BarState extends State<Nav_Bar> {
   var currentIndex = 0;
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       bottomNavigationBar: Container(
         margin: const EdgeInsets.all(20),
         height: screenWidth * .155,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.15),
+              blurRadius: 30,
+              offset: const Offset(0, 10),
+            )
+          ],
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: ListView.builder(
+          itemCount: 4,
+          scrollDirection: Axis.horizontal(Vert,
+          itemBuilder: (context, index) => InkWell(onTap: () {
+            setState(() {
+              currentIndex = index;
+              HapticFeedback.lightImpact();
+            });
+          }),
+        ),
       ),
     );
   }
