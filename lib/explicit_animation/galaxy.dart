a0ab3eec999e680e735c68d02f416fe70cf6c851
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-06 15:03:23 +0100

    Update Assets

---
+++ lib/explicit_animation/galaxy.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Rotating_Galaxy extends StatefulWidget {
+  const Rotating_Galaxy({super.key});
+
+  @override
+  State<Rotating_Galaxy> createState() => _Rotating_GalaxyState();
+}
+
+class _Rotating_GalaxyState extends State<Rotating_Galaxy> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
