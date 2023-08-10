918b6b1407e9385bc69580297bda3615c9793780
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-10 03:28:49 +0100

    Update animated_container.dart

--- lib/quick_practice/animated_container.dart
+++ lib/quick_practice/animated_container.dart
@@ -8,11 +8,51 @@ class AnimatedContainerStar extends StatefulWidget {
 }
 
 class _AnimatedContainerStarState extends State<AnimatedContainerStar> {
+  bool _bigger = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Implicit Animation'),
+        title: const Text(
+          'Implicit Animation',
+          style: TextStyle(color: Colors.white),
+        ),
+        elevation: 3,
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: AnimatedContainer(
+              width: _bigger ? 80 : 120,
+              duration: const Duration(seconds: 500),
+              curve: Curves.bounceIn,
+              child: Image.asset(
+                'assets/images/star.png',
+
+                // color: _bigger ? Colors.green : Colors.blue,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _bigger = !_bigger;
+              });
+            },
+            child: Container(
+                width: 100,
+                height: 30,
+                decoration: const BoxDecoration(color: Colors.blue),
+                child: const Text(
+                  'Enlargement',
+                  style: TextStyle(color: Colors.white),
+                )),
+          )
+        ],
       ),
     );
   }
