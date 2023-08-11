430ca5d927f520c59849351addc756d251ad23fc
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-11 20:27:06 +0100

    Update animated_container.dart

---
+++ lib/hey_flutter/animated_container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Animated_Container extends StatefulWidget {
+  const Animated_Container({super.key});
+
+  @override
+  State<Animated_Container> createState() => _Animated_ContainerState();
+}
+
+class _Animated_ContainerState extends State<Animated_Container> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
