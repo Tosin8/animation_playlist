520e60ebaf650965f2968d863198d4c7d942cdb0
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-11 22:23:21 +0100

    Update animated_icon.dart

--- lib/hey_flutter/animated_icon.dart
+++ lib/hey_flutter/animated_icon.dart
@@ -10,41 +10,61 @@ class Animated_Icon extends StatefulWidget {
 class _Animated_IconState extends State<Animated_Icon>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
+
+  bool isPlaying = false;
+
+  void toggleIcon() => setState(() {
+        isPlaying = !isPlaying;
+        isPlaying
+            ? _animationController.forward()
+            : _animationController.reverse();
+      });
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _animationController = AnimationController(
-      duration: const Duration(milliseconds: 13000),
+      duration: const Duration(seconds: 1),
       vsync: this,
-    )..forward().then((_) async {
-        await Future.delayed(const Duration(seconds: 1));
-        _animationController.reverse();
-      });
-  }
+    );
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Animated Icon',
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.black,
-        ),
-        body: Center(
-            child: AnimatedIcon(
-          icon: AnimatedIcons.play_pause,
-          size: 160,
-          progress: _animationController,
-        )));
+    // MAKING THE BELOW CODE TO BEING IN AUTO WITHOUT BEING PRESSD.
+    // ..forward().then((_) async {
+    //     await Future.delayed(const Duration(seconds: 1));
+    //     _animationController.reverse();
+    //   });
   }
 
   @override
   void dispose() {
     _animationController.dispose();
     super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Animated Icon',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: IconButton(
+          splashColor: Colors.green,
+          splashRadius: 3,
+          icon: AnimatedIcon(
+            icon: AnimatedIcons.play_pause,
+            size: 160,
+            progress: _animationController,
+          ),
+          onPressed: () {
+            toggleIcon();
+          },
+        ),
+      ),
+    );
   }
 }
