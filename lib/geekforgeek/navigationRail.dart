5bfe8da5cbeabb3d7ea3cc65fde9d6024732577b
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-15 21:34:08 +0100

    Create navigationRail.dart

---
+++ lib/geekforgeek/navigationRail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavigationRail extends StatefulWidget {
+  const NavigationRail({super.key});
+
+  @override
+  State<NavigationRail> createState() => _NavigationRailState();
+}
+
+class _NavigationRailState extends State<NavigationRail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
