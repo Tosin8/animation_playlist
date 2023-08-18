34cd5cab080622fdc4e47ad5d913985d20bd38b4
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-18 23:48:00 +0100

    Update gravity.dart

--- lib/animation_gallery/gravity.dart
+++ lib/animation_gallery/gravity.dart
@@ -12,33 +12,35 @@ class _GravityState extends State<Gravity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: InkWell(
-      splashColor: Colors.transparent,
-      highlightColor: Colors.transparent,
-      onTap: () => setState(() {
-        isTapped = !isTapped;
-      }),
-      child: Container(
-        margin: const EdgeInsets.only(bottom: 0),
-        height: 150,
-        width: 220,
-        decoration: BoxDecoration(
-            color: Color(0xffFF4e4e),
-            borderRadius: BorderRadius.all(
-              Radius.circular(20),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(.3),
-                blurRadius: 10,
-                offset: Offset(0, 7),
+        body: Center(
+      child: InkWell(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        onTap: () => setState(() {
+          isTapped = !isTapped;
+        }),
+        child: AnimatedContainer(
+          curve: isTapped ? Curves.bounceOut : Curves.ease,
+          duration: const Duration(milliseconds: 300),
+          margin: EdgeInsets.only(bottom: isTapped ? 0 : 400),
+          height: 150,
+          width: 220,
+          decoration: BoxDecoration(
+              color: Color(0xffFF4e4e),
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
               ),
-            ]),
-        child: Center(
-            child: Icon(Icons.arrow_downward,
-                // color: Colors.white,
-
-                size: 35)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(.3),
+                  blurRadius: 10,
+                  offset: Offset(0, 7),
+                ),
+              ]),
+          child: Center(
+              child: Icon(isTapped ? Icons.arrow_downward : Icons.arrow_upward,
+                  color: Colors.white, size: 35)),
+        ),
       ),
     ));
   }
