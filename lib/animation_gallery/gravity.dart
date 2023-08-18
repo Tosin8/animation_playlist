1ee2436aeb98b4d961f5eea9022a7547d5766440
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-18 22:55:01 +0100

    Update gravity.dart

---
+++ lib/animation_gallery/gravity.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Gravity extends StatefulWidget {
+  const Gravity({super.key});
+
+  @override
+  State<Gravity> createState() => _GravityState();
+}
+
+class _GravityState extends State<Gravity> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
