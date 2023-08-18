34380b6e4c36e1b9340b0645be4595e59c9989ee
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-18 22:48:36 +0100

    Update switch.dart

--- lib/animation_gallery/switch.dart
+++ lib/animation_gallery/switch.dart
@@ -13,17 +13,40 @@ class _SwitchAnimateState extends State<SwitchAnimate> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: InkWell(
-        splashColor: Colors.transparent,
-        highlightColor: Colors.transparent,
-        onTap: () => setState(() {
-          isTapped = !isTapped;
-        }),
-        child: Text(
-          isTapped ? 'OFF' : 'ON',
-          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 19),
+            child: InkWell(
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      onTap: () => setState(() {
+        isTapped = !isTapped;
+      }),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.fastLinearToSlowEaseIn,
+        height: isTapped ? 64 : 67,
+        width: isTapped ? 190 : 200,
+        decoration: BoxDecoration(
+            color: isTapped ? Color(0xffB23F3F) : Color(0xffFF4e4e),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(isTapped ? 0 : 0.3),
+                blurRadius: isTapped ? 0 : 10,
+                offset: Offset(0, 7),
+              )
+            ]),
+        child: Center(
+          child: Text(
+            isTapped ? 'OFF' : 'ON',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 19,
+              color: isTapped
+                  ? Colors.white.withOpacity(0.5)
+                  : Colors.white.withOpacity(0.9),
+            ),
+          ),
         ),
       ),
-    ));
+    )));
   }
 }
