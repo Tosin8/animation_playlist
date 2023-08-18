02645a304013d7e69d5f9ae2eff787b60a5dbcd9
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-18 23:25:45 +0100

    Update gravity.dart

--- lib/animation_gallery/gravity.dart
+++ lib/animation_gallery/gravity.dart
@@ -11,6 +11,35 @@ class _GravityState extends State<Gravity> {
   bool isTapped = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Icon(Icons.arrow_downward)));
+    return Scaffold(
+        body: InkWell(
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      onTap: () => setState(() {
+        isTapped = !isTapped;
+      }),
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 0),
+        height: 150,
+        width: 220,
+        decoration: BoxDecoration(
+            color: Color(0xffFF4e4e),
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(.3),
+                blurRadius: 10,
+                offset: Offset(0, 7),
+              ),
+            ]),
+        child: Center(
+            child: Icon(Icons.arrow_downward,
+                // color: Colors.white,
+
+                size: 35)),
+      ),
+    ));
   }
 }
