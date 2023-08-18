606ca2fcd62e3417a6f73ed21bc921395fd2b3f4
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-18 21:11:20 +0100

    Update animate_container.dart

---
+++ lib/implict_animation/animate_container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatingContainer extends StatefulWidget {
+  const AnimatingContainer({super.key});
+
+  @override
+  State<AnimatingContainer> createState() => _AnimatingContainerState();
+}
+
+class _AnimatingContainerState extends State<AnimatingContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
