3b9d897427dd779709f71864b1f42d0d844a5710
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-21 13:13:59 +0100

    Update screen_challenge.dart

--- lib/hey_flutter/screen_challenge.dart
+++ lib/hey_flutter/screen_challenge.dart
@@ -10,6 +10,18 @@ class Screen_Challenge extends StatefulWidget {
 class _Screen_ChallengeState extends State<Screen_Challenge> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'TasteMe',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white24,
+      ),
+      drawer: Drawer(),
+    );
   }
 }
