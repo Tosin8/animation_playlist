464a849e2471b83e2ec26fc973f4362318e54ba3
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-22 20:02:16 +0100

    Update screen_challenge.dart

--- lib/hey_flutter/screen_challenge/screen_challenge.dart
+++ lib/hey_flutter/screen_challenge/screen_challenge.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'component.dart';
-import 'tabs.dart';
 
 class Screen_Challenge extends StatefulWidget {
   const Screen_Challenge({super.key});
@@ -20,7 +19,12 @@ class _Screen_ChallengeState extends State<Screen_Challenge> {
               onPressed: () {
                 print('notification button bell was pressed');
               },
-              icon: const Icon(Icons.notifications_on))
+              icon: const Icon(Icons.notifications_on)),
+          IconButton(
+              onPressed: () {
+                print('Favorite button pressed');
+              },
+              icon: const Icon(Icons.favorite_border_outlined)),
         ],
         title: const Text(
           'TasteMe',
@@ -79,7 +83,29 @@ class _Screen_ChallengeState extends State<Screen_Challenge> {
           textAlign: TextAlign.center,
         ),
       ]),
-      bottomNavigationBar: BottomNavigationBar(items: []),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 3,
+        //iconSize: 30,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.verified),
+            label: 'Profile',
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+      ),
     );
   }
 }
