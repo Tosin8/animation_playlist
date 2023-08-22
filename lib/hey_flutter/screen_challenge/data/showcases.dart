1f3839b1760799e79971414f056084c050dc1581
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-22 21:58:56 +0100

    Create showcases.dart

---
+++ lib/hey_flutter/screen_challenge/data/showcases.dart
@@ -0,0 +1,24 @@
+import '../model/product_showcase.dart';
+
+const List<Showcase> showcases = [
+  Showcase(
+      image: 'assets/images/food.jpg',
+      title: 'Leto Caffe, London, United Kingdom',
+      owner: 'tasteme'),
+  Showcase(
+      image: 'assets/images/food1.jpg',
+      title: 'Italian dish, Roma',
+      owner: 'tasteme'),
+  Showcase(
+      image: 'assets/images/food2.jpg',
+      title: 'Romania Food, Recipe',
+      owner: 'tasteme'),
+  Showcase(
+      image: 'assets/images/pizza.jpg',
+      title: 'Leto Caffe, London, United Kingdom',
+      owner: 'tasteme'),
+  Showcase(
+      image: 'assets/images/pancake.jpg',
+      title: 'Leto Caffe, London, United Kingdom',
+      owner: 'tasteme'),
+];
