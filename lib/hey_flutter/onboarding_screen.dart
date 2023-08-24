f3c03f7ffc64caa16a2f441c6cf05264e3ffba24
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-24 19:59:19 +0100

    Update onboarding_screen.dart

--- lib/hey_flutter/onboarding_screen.dart
+++ lib/hey_flutter/onboarding_screen.dart
@@ -10,29 +10,34 @@ class OnBoardingPage extends StatefulWidget {
 class _OnBoardingPageState extends State<OnBoardingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        children: [
-          Container(
-              color: Colors.red,
-              child: const Center(
-                  child: Text(
-                'Page 1',
-                style: TextStyle(color: Colors.white),
-              ))),
-          Container(
-              color: Colors.green,
-              child: const Center(
-                  child:
-                      Text('Page 2', style: TextStyle(color: Colors.white)))),
-          Container(
-              color: Colors.blue,
-              child: const Center(
-                  child: Text(
-                'Page 3',
-                style: TextStyle(color: Colors.white),
-              ))),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.only(bottom: 80),
+          child: PageView(
+            children: [
+              Container(
+                  color: Colors.red,
+                  child: const Center(
+                      child: Text(
+                    'Page 1',
+                    style: TextStyle(color: Colors.white),
+                  ))),
+              Container(
+                  color: Colors.green,
+                  child: const Center(
+                      child: Text('Page 2',
+                          style: TextStyle(color: Colors.white)))),
+              Container(
+                  color: Colors.blue,
+                  child: const Center(
+                      child: Text(
+                    'Page 3',
+                    style: TextStyle(color: Colors.white),
+                  ))),
+            ],
+          ),
+        ),
       ),
     );
   }
