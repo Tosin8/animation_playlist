84f5318b94760bea3bf0099e1d0c1c7763d79d24
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-24 20:13:58 +0100

    Updating with PageController

--- lib/hey_flutter/onboarding_screen.dart
+++ lib/hey_flutter/onboarding_screen.dart
@@ -8,6 +8,14 @@ class OnBoardingPage extends StatefulWidget {
 }
 
 class _OnBoardingPageState extends State<OnBoardingPage> {
+  final controller = PageController();
+  @override
+  void dispose() {
+    controller.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
