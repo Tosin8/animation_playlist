16b379923203504370185e0e09115f3c34439a82
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-26 00:39:19 +0100

    Update animated_switcher.dart

--- lib/hey_flutter/animated_widget/animated_switcher.dart
+++ lib/hey_flutter/animated_widget/animated_switcher.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class widget7 extends StatefulWidget {
@@ -8,31 +10,41 @@ class widget7 extends StatefulWidget {
 }
 
 class _widget7State extends State<widget7> {
-  int _count = 0; 
+  int _count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
-          toolbarHeight: 80,
-          centerTitle: true,
-          title: Text(
-            'Animated Switcher',
-            style: TextStyle(color: Colors.white),
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        toolbarHeight: 80,
+        centerTitle: true,
+        title: Text(
+          'Animated Switcher',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              AnimatedSwitcher(
-                duration: Duration(milliseconds: 500), 
-                child: Text(
-                  '$_count', 
-                style: const TextStyle(
-                  fontSize: 40), 
-                  key: ValueKey(_count),
-                ),),)],
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          AnimatedSwitcher(
+            duration: Duration(milliseconds: 500),
+            child: Text(
+              '$_count',
+              style: TextStyle(fontSize: 40),
+              key: ValueKey(_count),
+            ),
+            transitionBuilder: (child, animation) {
+              return ScaleTransition(scale: animation, child: child);
+            },
           ),
-        ));
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _count += 1;
+                });
+              },
+              child: Text('Add')),
+        ]),
+      ),
+    );
   }
 }
