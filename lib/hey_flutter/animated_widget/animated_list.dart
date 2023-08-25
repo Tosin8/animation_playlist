e343e234c94191bbb7ee8178553e42873ad7df29
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-25 23:33:10 +0100

    Update animated_list.dart

--- lib/hey_flutter/animated_widget/animated_list.dart
+++ lib/hey_flutter/animated_widget/animated_list.dart
@@ -9,7 +9,38 @@ class widget5 extends StatefulWidget {
 
 class _widget5State extends State<widget5> {
   final _items = [];
-  final GlobalKey<AnimatedList> _key = GlobalKey();
+  final GlobalKey<AnimatedListState> _key = GlobalKey();
+
+  void _addItem() {
+    _items.insert(0, 'Item ${_items.length + 1}');
+    _key.currentState!.insertItem(
+      0,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  void _removeItem(int index) {
+    _key.currentState!.removeItem(
+      index,
+      (_, animation) {
+        return SizeTransition(
+          sizeFactor: animation,
+          child: const Card(
+            margin: const EdgeInsets.all(10),
+            color: Colors.red,
+            child: ListTile(
+              title: Text(
+                'Deleted',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+        );
+      },
+      duration: const Duration(seconds: 3),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
