0fe995295dcd8772748f1c3d2252ec1e843cd7c3
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-25 21:30:51 +0100

    Update animated_crossfade.dart

--- lib/hey_flutter/animated_widget/animated_crossfade.dart
+++ lib/hey_flutter/animated_widget/animated_crossfade.dart
@@ -8,13 +8,31 @@ class widget3 extends StatefulWidget {
 }
 
 class _widget3State extends State<widget3> {
+  bool _bool = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 3,
-        title: Text('Animated Crossfade'),
-      ),
-    );
+        appBar: AppBar(
+          elevation: 3,
+          title: Text('Animated Crossfade'),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 100,
+            ),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    _bool = !_bool;
+                  });
+                },
+                child: Text(
+                  'Switch',
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ));
   }
 }
