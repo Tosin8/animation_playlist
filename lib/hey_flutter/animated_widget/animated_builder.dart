6ca766302e957e717dfd5f6850bceb1adfc455ff
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-25 21:14:33 +0100

    Update animated_builder.dart

--- lib/hey_flutter/animated_widget/animated_builder.dart
+++ lib/hey_flutter/animated_widget/animated_builder.dart
@@ -9,7 +9,16 @@ class widget2 extends StatefulWidget {
 
 class _widget2State extends State<widget2> with TickerProviderStateMixin {
   late final AnimationController _controller =
-      AnimationController(vsync: this, duration: const Duration(seconds: 3));
+      AnimationController(vsync: this, duration: const Duration(seconds: 3))
+        ..repeat();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
