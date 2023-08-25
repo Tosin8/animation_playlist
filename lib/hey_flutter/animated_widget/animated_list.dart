a89fd7cdee5c46dffd12f3732cc9f58c29bc3443
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-26 00:10:35 +0100

    Create animated_rotation

--- lib/hey_flutter/animated_widget/animated_list.dart
+++ lib/hey_flutter/animated_widget/animated_list.dart
@@ -15,7 +15,7 @@ class _widget5State extends State<widget5> {
     _items.insert(0, 'Item ${_items.length + 1}');
     _key.currentState!.insertItem(
       0,
-      duration: const Duration(seconds: 1),
+      duration: const Duration(milliseconds: 300),
     );
   }
 
@@ -37,18 +37,51 @@ class _widget5State extends State<widget5> {
           ),
         );
       },
-      duration: const Duration(seconds: 3),
+      duration: const Duration(seconds: 1),
     );
+    _items.removeAt(index);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Animated List'),
+          title: const Text('Animated List'),
         ),
         body: Column(
-          children: [],
+          children: [
+            const SizedBox(height: 10),
+            IconButton(
+              onPressed: _addItem,
+              icon: const Icon(Icons.add),
+            ),
+            Expanded(
+                child: AnimatedList(
+              key: _key,
+              initialItemCount: 0,
+              itemBuilder: (context, index, animation) {
+                return SizeTransition(
+                  key: UniqueKey(),
+                  sizeFactor: animation,
+                  child: Card(
+                      margin: EdgeInsets.all(10),
+                      color: Colors.orangeAccent,
+                      child: ListTile(
+                        title: Text(
+                          '_items[index]',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            _removeItem(index);
+                          },
+                        ),
+                      )),
+                );
+              },
+            ))
+          ],
         ));
   }
 }
