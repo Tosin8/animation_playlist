32691878a13b820e9a4689be1f98c049f9a23751
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-06 07:37:31 +0100

    Update inherited_widget.dart

--- lib/vandad/state_mgt/inherited_widget.dart
+++ lib/vandad/state_mgt/inherited_widget.dart
@@ -7,18 +7,25 @@ class name extends StatefulWidget {
   State<name> createState() => _nameState();
 }
 
+String title = 'tap the scren';
+
 class _nameState extends State<name> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Home Page',
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          title: Text(
+            title,
+            style: const TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.black,
         ),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-      ),
-    );
+        body: GestureDetector(
+          onTap: () {
+          setState(() {
+            title = DateTime.now().toIso8601String();
+          });
+        }));
   }
 }
