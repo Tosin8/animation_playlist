1fef4dcd5a3d4ba010351fdf82c390939519ab6a
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-06 16:55:11 +0100

    Copying code into inherited model file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'vandad/state_mgt/inherited_model.dart';
+
 // Future main() async {
 //   WidgetsFlutterBinding.ensureInitialized();
 
@@ -75,38 +77,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class inherit_model extends StatefulWidget {
-  const inherit_model({super.key});
-
-  @override
-  State<inherit_model> createState() => _inherit_modelState();
-}
-
-class _inherit_modelState extends State<inherit_model> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text(
-            'Inherit Model',
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.black45),
-    );
-  }
-}
-
-// creating an array of colors.
-final colors = [
-  Colors.blue,
-  Colors.red,
-  Colors.yellow,
-  Colors.orange,
-  Colors.cyan,
-  Colors.purple,
-  Colors.brown,
-  Colors.amber,
-  Colors.deepPurple,
-];
---
+++ lib/vandad/state_mgt/inherited_model.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class inherit_model extends StatefulWidget {
+  const inherit_model({super.key});
+
+  @override
+  State<inherit_model> createState() => _inherit_modelState();
+}
+
+class _inherit_modelState extends State<inherit_model> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+            'Inherit Model',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.black45),
+    );
+  }
+}
+
+// creating an array of colors.
+final colors = [
+  Colors.blue,
+  Colors.red,
+  Colors.yellow,
+  Colors.orange,
+  Colors.cyan,
+  Colors.purple,
+  Colors.brown,
+  Colors.amber,
+  Colors.deepPurple,
+];
+
+// grabbing random colors elements. 
