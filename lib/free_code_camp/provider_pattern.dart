f9daea38c36ba47d303d06c08d3bf1b4835c8d1d
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-08 19:42:43 +0100

    Update provider_pattern.dart

--- lib/free_code_camp/provider_pattern.dart
+++ lib/free_code_camp/provider_pattern.dart
@@ -8,17 +8,28 @@ class CounterTimer extends StatefulWidget {
 }
 
 class _CounterTimerState extends State<CounterTimer> {
+  int counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          'Counter Timer',
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            'Counter Timer',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-    );
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
+        )));
   }
 }
