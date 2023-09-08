7eae1c64dcb3fd895cc1097a61a9bf6c5ef2a365
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-08 20:29:05 +0100

    Update provider_pattern.dart

--- lib/free_code_camp/provider_pattern.dart
+++ lib/free_code_camp/provider_pattern.dart
@@ -3,11 +3,10 @@ import 'package:provider/provider.dart';
 
 // ignore: must_be_immutable
 class CounterTimer extends StatelessWidget {
-  const CounterTimer({super.key});
+  //const CounterTimer({super.key});
 
   int counter = 0;
 
-  void incrementCounter() {}
   void incrementCounter(BuildContext context) {
     Provider.of<Counter>(context, listen: false).incrementCounter();
   }
@@ -37,7 +36,7 @@ class CounterTimer extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: incrementCounter,
+          onPressed: () => incrementCounter(context),
           tooltip: 'Increments',
           child: Icon(Icons.add)),
     );
