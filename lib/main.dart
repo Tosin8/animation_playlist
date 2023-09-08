7f67d198dff7ca980b35f411bdb30aba308720ef
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-08 21:22:04 +0100

    Creating Class in Widget Stateless

--- lib/main.dart
+++ lib/main.dart
@@ -151,13 +151,4 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
-    );
-  }
-}
