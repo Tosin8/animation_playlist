9af9e328e4e384709cf49260d6515b34da37c12a
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-10 21:41:00 +0100

    Update orientation_query.dart

---
+++ lib/hey_flutter/orientation_query.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Orientation_bld extends StatefulWidget {
+  const Orientation_bld({super.key});
+
+  @override
+  State<Orientation_bld> createState() => _Orientation_bldState();
+}
+
+class _Orientation_bldState extends State<Orientation_bld> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
