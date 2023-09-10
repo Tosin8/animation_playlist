d7421ae08d1e08fc499f8b2798081609e232d3c9
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-10 21:58:31 +0100

    Update orientation_query.dart

--- lib/hey_flutter/orientation_query.dart
+++ lib/hey_flutter/orientation_query.dart
@@ -11,18 +11,20 @@ class _Orientation_bldState extends State<Orientation_bld> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Orientation"),
-        ),
-        body: MediaQuery.of(context).orientation == Orientation.portrait
-            ? Container(
-                color: Colors.blue,
-                child: const Center(child: Text('Portrait')))
-            : Container(
-                color: Colors.green,
-                child: const Center(
-                  child: Text('Landscape'),
-                ),
-              ));
+      appBar: AppBar(
+        title: const Text("Orientation"),
+      ),
+      body: MediaQuery.of(context).orientation == Orientation.portrait
+          ? Container(
+              color: Colors.blue,
+              child: const Center(child: Text('Portrait')),
+            )
+          : Container(
+              color: Colors.green,
+              child: const Center(
+                child: Text('Landscape'),
+              ),
+            ),
+    );
   }
 }
