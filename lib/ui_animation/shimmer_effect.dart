ba6e444df6e3b3257fc640cadf0ec09e8a34a5f8
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-18 17:19:49 +0100

    Update shimmer_effect.dart

--- lib/ui_animation/shimmer_effect.dart
+++ lib/ui_animation/shimmer_effect.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class ShimmerEffects extends StatefulWidget {
+  const ShimmerEffects({super.key});
+
+  @override
+  State<ShimmerEffects> createState() => _ShimmerEffectsState();
+}
+
+class _ShimmerEffectsState extends State<ShimmerEffects> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
