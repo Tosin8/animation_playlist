702a69bfd2421557dcc1c5adea9f344a5192c56e
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-26 22:09:45 +0100

    Update rough_practice.dart

--- lib/rough_practice.dart
+++ lib/rough_practice.dart
@@ -7,22 +7,30 @@ class RoughPractice extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Rough Practice'),
+          title: const Text('Rough Practice'),
         ),
         body: CustomScrollView(
           slivers: [
             SliverList(
                 delegate: SliverChildListDelegate([
-              ListTile(title: Text('First Item')),
-              ListTile(title: Text('Second Item')),
-              ListTile(title: Text('Third Item')),
-              ListTile(title: Text('Fourth Item')),
+              const ListTile(title: Text('First Item')),
+              const ListTile(title: Text('Second Item')),
+              const ListTile(title: Text('Third Item')),
+              const ListTile(title: Text('Fourth Item')),
+              const ListTile(title: Text('First Item')),
+              const ListTile(title: Text('Second Item')),
+              const ListTile(title: Text('Third Item')),
+              const ListTile(title: Text('Fourth Item')),
+              const ListTile(title: Text('First Item')),
+              const ListTile(title: Text('Second Item')),
+              const ListTile(title: Text('Third Item')),
+              const ListTile(title: Text('Fourth Item')),
             ])),
             SliverFillRemaining(
               hasScrollBody: false,
               child: Container(
                   color: Colors.yellowAccent,
-                  child: Column(
+                  child: const Column(
                     children: [
                       FlutterLogo(size: 100),
                       SizedBox(height: 20),
