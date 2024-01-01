61c2cd496bd18e2cd49c488298c3b82177941e5b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-01 23:00:34 +0100

    Refactoring

--- lib/implict_animation/tween_builder/star_1.dart
+++ lib/implict_animation/tween_builder/star_1.dart
@@ -12,26 +12,47 @@ class _Star_1State extends State<Star_1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Tween Builder'),
-      ),
-      body: Column(
-        children: [
-          Center(
-              child: TweenAnimationBuilder(
-            duration: const Duration(milliseconds: 300),
-            tween: Tween<double>(begin: 0, end: 2 * math.pi),
-            builder: (_, double angle, __) {
-              return Transform.rotate(
-                angle: angle,
-                child: Image.network(
-                    'https://images.unsplash.com/photo-1671600939110-7b0625c25ef5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80'),
-              );
-            },
-          ))
-        ],
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.blue,
+      //   title: const Text('Tween Builder'),
+      // ),
+      body: Stack(
+        
+        children:[ 
+          Container(
+            width: double.infinity, height: double.infinity,
+            child: Image.asset('assets/images/galaxy.jpg', 
+            fit: BoxFit.cover, 
+            ),
+          
+          ), 
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, 
+            horizontal: 30), 
+            child: Column(
+            children: [
+              // Center(
+              //     child: TweenAnimationBuilder(
+              //   duration: const Duration(milliseconds: 300),
+              //   tween: Tween<double>(begin: 0, end: 2 * math.pi),
+              //   builder: (_, double angle, __) {
+              //     return Transform.rotate(
+              //       angle: angle,
+              //       child: Image.network(
+              //           'https://images.unsplash.com/photo-1671600939110-7b0625c25ef5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80'),
+              //     );
+              //   },
+              // ))
+                    
+              
+               Image.asset('assets/images/sun.png'),
+              
+            ],
+                    
+            
+                    ),
+          ),
+     ] ),
     );
   }
 }
