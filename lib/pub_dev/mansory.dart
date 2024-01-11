06cc7ca5a1551310ec39695c36eb8b1721e46e26
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-11 16:45:22 +0100

    Creating page for discover

--- lib/pub_dev/mansory.dart
+++ lib/pub_dev/mansory.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_masonry_view/flutter_masonry_view.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-class MansoryView extends StatelessWidget {
+class MansoryView extends StatefulWidget {
   const MansoryView({super.key});
 
+  @override
+  State<MansoryView> createState() => _MansoryViewState();
+}
 
+class _MansoryViewState extends State<MansoryView> {
 final _items = [
   'assets/images/food.jpg', 
   'assets/images/food1.jpg', 
@@ -14,8 +20,23 @@ final _items = [
   'assets/images/pancake.jpg', 
   'assets/images/spaghetti.jpg'
 ]; 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mansory View'), 
+      ),
+      body: SingleChildScrollView(
+        child: MasonryView(
+          listOfItem: _items,
+          numberOfColumn: 2, 
+          itemBuilder: (_items) {
+            return Image.asset(_items);
+          },
+        ), 
+
+      )
+    );
   }
 }
